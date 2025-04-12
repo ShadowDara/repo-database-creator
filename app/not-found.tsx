12b937baa190224c8f3648f48f1c59d42710e19f
ShadowDara <128976697+ShadowDara@users.noreply.github.com>
2025-04-12 11:26:51 +0200

    add title

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,10 +1,15 @@
 // app/not-found.tsx
 
+export const metadata = {
+    title: "404 Not Found - Repo Database Creator!",
+    description: "This Page does not exist Anymore!",
+};
+
 export default function NotFound() {
     return (
       <div className="h-screen flex flex-col items-center justify-center text-center p-8">
         <h1 className="text-8xl font-bold mb-4">404</h1>
-        <p className="text-lg text-gray-600 mb-6">
+        <p className="text-lg mb-6">
           This Page does not exist!
         </p>
         <a href="/" className="text-blue-500 hover:underline">
