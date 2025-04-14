c7c7b3247d3223b642e3d32ae3b7250788dae72b
ShadowDara <128976697+ShadowDara@users.noreply.github.com>
2025-04-14 15:31:46 +0200

    Update page.tsx

--- app/api/repos/page.tsx
+++ app/api/repos/page.tsx
@@ -106,7 +106,7 @@ export default async function ReposPage({
                     </tr>
                   ))}
                 </tbody>
-k
+
               </table>
             )}
           </div>
