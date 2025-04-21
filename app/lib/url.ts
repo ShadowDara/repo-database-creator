340ca0553885bea11411f1d03c3e7e82c7d01e3c
Shadowdara <128976697+ShadowDara@users.noreply.github.com>
2025-04-21 15:16:07 +0200

    Update url.ts

--- app/lib/url.ts
+++ app/lib/url.ts
@@ -2,7 +2,7 @@
 import { promises as fs } from 'fs'
 import path from 'path'
 
-const themesPath = path.join(process.cwd(), 'app/lib/settings.json')
+const settingsPath = path.join(process.cwd(), 'app/lib/settings.json')
 
 export function getSearchParams(request: Request) {
     const { searchParams } = new URL(request.url);
@@ -20,8 +20,11 @@ export function getSearchParams(request: Request) {
 }
 
 export async function getRepoCount(user: string): Promise<number | null> {
+  const file = await fs.readFile(settingsPath, 'utf-8')
+  const json = JSON.parse(file)
+
   const response = await fetch(`https://api.github.com/users/${user}`, {
-    next: { revalidate: 1 }, // in seconds
+    next: { revalidate: json['cache-time'] }, // in seconds
     cache: "force-cache",
   })
 
