7b1aef4f96979f726df5fbdaa3531be2e4d6e823
Shadowdara <128976697+ShadowDara@users.noreply.github.com>
2025-04-23 00:43:55 +0200

    Update route.ts

--- app/api/svg/repo_count/route.ts
+++ app/api/svg/repo_count/route.ts
@@ -12,7 +12,7 @@ export async function GET(request: Request) {
   try {
     const themes = await loadThemes()
 
-    const theme = (themes as ThemeMap)[themeName];
+    //const theme = (themes as ThemeMap)[themeName];
     
     const repoCount = await getRepoCount(user)
 
