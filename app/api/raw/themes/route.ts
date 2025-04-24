089b09d4ca2b34db36384b0af49b99bbfe4850a3
Shadowdara <128976697+ShadowDara@users.noreply.github.com>
2025-04-24 18:34:35 +0200

    Update route.ts

--- app/api/raw/themes/route.ts
+++ app/api/raw/themes/route.ts
@@ -1,10 +1,9 @@
 // app/api/raw/themes/route.ts
 
-import { NextResponse } from "next/server";
 import { themes } from '../../../lib/themes';
 
 export async function GET(request: Request) {
-    return new NextResponse(themes, {
+    return new Response(JSON.stringify(themes), {
         status: 200,
         headers: { "Content-Type": "text/plain" },
     });
