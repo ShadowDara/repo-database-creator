83d5aa7b236f3c47dbe1f7aab95c9d23ca6dc93a
Shadowdara <128976697+ShadowDara@users.noreply.github.com>
2025-05-10 17:58:00 +0200

    Update route.ts

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -7,7 +7,7 @@ import handler from '@/app/lib/timer';
 
 export async function GET(req: NextRequest) {
   const secret = req.headers.get('CRON_SECRET');
-  
+  handler();
   if (secret !== process.env.CRON_SECRET) {
     return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
   }
