2819ec083949b711f81b675a2829b6c4f5a4ed5b
Shadowdara <128976697+ShadowDara@users.noreply.github.com>
2025-05-10 17:43:12 +0200

    tets

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -6,7 +6,7 @@ import { NextRequest, NextResponse } from 'next/server';
 import handler from '@/app/lib/timer';
 
 export async function GET(req: NextRequest) {
-  const secret = req.headers.get('x_cron_secret');
+  const secret = req.headers.get('CRON_SECRET');
   
   if (secret !== process.env.CRON_SECRET) {
     return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
